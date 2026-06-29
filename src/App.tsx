e2bf85657e4e1f77a059b2f6df481e97e333681b
mohammedabdelnasser884-hash <mohammedabdelnasser884@gmail.com>
2026-06-30 01:16:41 +0300

    Add src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -561,6 +561,7 @@ function App() {
             onClose: () => setShowNewSessionModal(false),
             onSaved: () => { fetchTodaySessions(); fetchUpcomingSessions(); fetchCases(0, casesFilter); },
             onNotify: sendTelegram,
+            profile,
         }),
         showLawyerModal && React.createElement(NewLawyerModal, { onClose: () => setShowLawyerModal(false), onSave: handleSaveLawyer, loading: savingLawyer }),
         showClientModal && React.createElement(NewClientModal, { onClose: () => setShowClientModal(false), onSave: handleSaveClient, loading: savingClient }),
